d8ffddbb849fac34f57ef306a7d1078b174d5da5
Luiz Henrique <luizhjramos@outlook.com>
2022-02-22 13:11:34 -0300

    feat: Purple Button Stitches variant

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ import type { NextPage } from "next";
 // Stitches styles
 import { css } from "@stitches/core";
 
+const body = css({ padding: 40 });
+
 const button = css({
   appearance: "none",
   border: "none",
@@ -15,15 +17,31 @@ const button = css({
 
   borderRadius: "9999px",
   backgroundColor: "Gainsboro",
+
+  "&:hover": {
+    backgroundColor: "LightGray",
+  },
+
+  variants: {
+    variant: {
+      purple: {
+        backgroundColor: "BlueViolet",
+        "&:hover": {
+          backgroundColor: "DarkViolet",
+        },
+      },
+    },
+  },
 });
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className={body()}>
       <Head>
         <title>Stitches and Radix</title>
       </Head>
       <button className={button()}>Salvar</button>
+      <button className={button({ variant: "purple" })}>Button Purple</button>
     </div>
   );
 };
