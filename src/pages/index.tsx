1c8a31a9aec8069cfd408e54bf92efda85f8dc38
Luiz Henrique <luizhjramos@outlook.com>
2022-02-22 08:08:14 -0300

    refact: cleaning application

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Stitches and Radix</title>
+      </Head>
+      <h1>Hello there</h1>
+    </div>
+  );
+};
+
+export default Home;
